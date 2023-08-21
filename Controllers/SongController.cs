24f5d532bf724fbd6aab790a53252ebeae27f426
Jaron <Jaronleach98@gmail.com>
2023-08-21 11:50:22 -0400

    uploading starter code

---
+++ Controllers/SongController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicAPIJaron.Data;
+using Microsoft.EntityFrameworkCore;
+using MusicAPIJaron.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicAPIJaron.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public SongController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<SongController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var Songs = _context.Songs.ToList();
+            return Ok(Songs);
+        }
+
+        // GET api/<SongController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var Song = _context.Songs.Find(id);
+            if (Song == null)
+            {
+                return NotFound();
+            }
+            return Ok(Song);
+        }
+
+        
+             
+        
+
+        // POST api/<SongController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Song Song)
+        {
+            _context.Songs.Add(Song);
+            _context.SaveChanges();
+            return StatusCode(201, Song);
+        }
+
+        // PUT api/<SongController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Song Song)
+        {
+            
+        }
+
+        // DELETE api/<SongController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+
+        {
+            var Song = _context.Songs.Find(id);
+            if (Song == null)
+            {
+                return NotFound();
+            }
+            _context.Songs.Remove(Song);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAPIJaron.Models;
+
+namespace MusicAPIJaron.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        
+        
+
+        
+    }
+}
